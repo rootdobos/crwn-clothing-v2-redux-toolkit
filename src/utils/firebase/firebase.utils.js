a1985a7a75ede0c2ca96896ed4b5170f2486b4c0
rootdobos <49080519+rootdobos@users.noreply.github.com>
2024-02-25 20:47:55 +0200

    used redux toolkit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyB29n1wmFoBzLWBw3p5XQta0AKUXLpSfyM",
+  authDomain: "crwn-clothing-db-c8f34.firebaseapp.com",
+  projectId: "crwn-clothing-db-c8f34",
+  storageBucket: "crwn-clothing-db-c8f34.appspot.com",
+  messagingSenderId: "302595713545",
+  appId: "1:302595713545:web:b262c369f83c076243a6d8"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
